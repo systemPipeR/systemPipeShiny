ac1eb3cb7b9c34a741de29c71a84dc5200478497
lz <lzhan140@ucr.edu>
2020-04-21 23:24:44 -0700

    TableHref

--- R/spsUIcollections.R
+++ R/spsUIcollections.R
@@ -4,9 +4,19 @@
 
 
 
-# input with a "X" button in the end to clear the entire typed text 
-# work the same as Textinput
-# must be used together with the js and css file
+#' Input component with a "X" button in the end to clear the entire typed text
+#' work the same as `Textinput`
+#' must be used together with the js and css file
+#' 
+#' @param inputId ID
+#' @param label text label above
+#' @param value default value
+#' @param placeholder place holder text
+#'
+#' @return a div
+#' @export
+#'
+#' @examples
 clearableTextInput <- function(inputId, label, value = "", placeholder = ""){
     tagList(tags$div(
         tags$label(label, `for` = inputId),
@@ -41,15 +51,17 @@ textInputGroup <- function(textId, btnId, title="", label="", icon = "paper-plan
 #' A shiny gallery component
 #' `texts`, `hrefs`, `images` Must have the same length
 #' Must be used with Bootstrap3 and sps.css file
+#'
 #' @param Id ID of this gallery
 #' @param title Title of gallery
 #' @param texts label under each image
 #' @param hrefs link when clicking each
+#' @param image_frame_size integer, 1-12
+#' @param img_height css style like '300px'
+#' @param img_width css style like '480px'
 #' @param images image source, 
-#' @param image_size size of each image 1 to 12
 #'
 #' @return a div element
-#' @export
 #'
 #' @examples
 #' texts <- c("p1", "p2", "p3", "p4", "p5")
@@ -75,22 +87,106 @@ textInputGroup <- function(textId, btnId, title="", label="", icon = "paper-plan
 #' }
 #' 
 #' shinyApp(ui, server)
-gallery <- function(Id = NULL, title = "Gallery", texts, hrefs, images, image_size = 4){
-    if (is.null(Id)) Id <- glue("gallery{sample(1000:10000, 1)}")
+gallery <- function(Id = NULL, title = "Gallery", title_color = "#0275d8", texts, hrefs, images, image_frame_size = 4, img_height = "300px", img_width = "480px"){
+    if (is.null(Id)) Id <- glue("gallery{sample(1000:100000, 1)}")
     assert_that(length(texts) == length(hrefs) & length(hrefs) == length(images), 
-                msg = "texts hrefs and images must have the same length")
+                msg = "texts, hrefs and images must have the same length")
     tags$div(
         id = Id, class = "col",
-        p(class = "text-center h2", title),
+        p(class = "text-center h2", style = glue("color: {title_color};"), title),
         tags$div(
             class = "row",
             HTML(glue('
-                <a href="{hrefs}"  class="col-sm-{image_size} style="right: 5px;" >
-                  <img src="{images}" class="img-gallery">
+                <a href="{hrefs}"  class="col-sm-{image_frame_size} style="right: 5px;" >
+                  <img src="{images}" class="img-gallery" style="height: {img_height}; width: {img_width};">
                   <p class="text-center h4">{texts}</p>
                 </a>
              '))
                  )
-    ) %>% return()
+    )
 }
 
+
+#' Show a list of tabs in buttons
+#' `label_text`, `hrefs` must be the same length
+#' @param Id optional
+#' @param title Item title
+#' @param title_color title color
+#' @param label_text individual tab labels
+#' @param hrefs individual tab links
+#'
+#' @return a div element
+#' @export
+#'
+#' @examples
+#' ui <- fluidPage(
+#' includeCSS("www/sps.css"),
+#' TabHref(label_text = c("Bar Plot", "PCA Plot", "Scatter Plot"), hrefs = c("https://google.com/", "b", "c"))
+#' )
+#' 
+#' server <- function(input, output, session) {
+#' 
+#' }
+#' 
+#' shinyApp(ui, server)
+TabHref <- function(Id = NULL, title = "A list of tabs", title_color = "#0275d8", label_text, hrefs, ...) {
+    if (is.null(Id)) Id <- glue("list-tab{sample(1000:100000, 1)}")
+    assert_that(length(label_text) == length(hrefs), 
+                msg = "texts and hrefs must have the same length")
+    tags$div(
+        id = Id, class = "col tab-group", ... ,
+        p(class = "text-center h4",  style = glue("color: {title_color}; text-align: left;"), title),
+        tags$div(
+            HTML(glue('
+                <a href="{hrefs}" class="tab-href">{label_text}</a>\n
+                '))
+        )
+    )
+}
+
+
+#' A table of lists of hyper reference buttons 
+#' `item_titles`, `item_labels`, `item_hrefs` must have the same length
+#' nth item in `item_labels`, `item_hrefs` must have the same length
+#' @param Id optional
+#' @param title title of this table
+#' @param text_color text color
+#' @param item_titles vector string, a vector of titles for table items
+#' @param item_labels list, a list of button lables in each table item
+#' @param item_hrefs list, a llist of hrefs for each button
+#' @param ... other HTML args
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' library(shiny)
+#' 
+#' ui <- fluidPage(
+#'     includeCSS("www/sps.css"),
+#'     TableHref(item_titles = c("worflow 1", "workflow 2"),
+#'               item_labels = list(c("tab 1", "tab 2"), c("tab 3", "tab 4")),
+#'               item_hrefs = list(c("https://www.google.com/", ""), c("", "")),
+#'     )
+#'     
+#' )
+#' 
+#' server <- function(input, output, session) {
+#'     
+#' }
+#' 
+#' shinyApp(ui, server)
+TableHref <- function(Id = NULL, title = "A Table of list of tabs", text_color = "#0275d8", item_titles, item_labels, item_hrefs, ...) {
+    if (is.null(Id)) Id <- glue("list-table{sample(1000:100000, 1)}")
+    assert_that(is.list(item_labels)); assert_that(is.list(item_hrefs))
+    assert_that(length(item_titles) == length(item_labels) & length(item_labels) == length(item_hrefs), 
+                msg = "item_titles, item_labels and item_hrefs must have the same length")
+    tags$div(
+        id = Id, class = "col",
+        p(class = "text-center h2",  style = glue("color: {text_color};"), title),
+        tags$hr(style="height:1px; background-color:gray;"),
+        lapply(1:length(item_titles), function(n) {
+            TabHref(title = item_titles[n], title_color = text_color, label_text = item_labels[[n]], hrefs = item_hrefs[[n]], class = "table-href")
+        })    
+    )
+}    
--- R/tab_vs_main.R
+++ R/tab_vs_main.R
@@ -19,24 +19,45 @@ gallery_plot <- data.frame(
     )
 )
 
-
+tabble_workflow <- data.frame(
+    Workflow = c(
+        ""
+    ),
+    hrefs = c(
+        ""
+    )
+    
+)
 ## UI
 vs_mainUI <- function(id){
     ns <- NS(id)
     tagList(
-        h2("VS main"),
-        p("Instructions on how to use the vs utilities."),
-        h4("For different workflows, you can choose data types to work with plotting"),
-        p("RNAseq: "),
-        a(href = "#shiny-tab-df_raw",  "Raw Count"),
-        a(href = "",  "df2"),
-        a(href = "",  "df3"),
+        HTML('
+        <h1 style="color: #5e9ca0;">Visualization&nbsp;</h1>
+        <p><strong>This is the visualization module. The main purpose is to help 
+        you quickly generate some plots.&nbsp;</strong></p>
+        <p><strong>There are <em>two</em> ways you can use this tab:</strong></p>
+        <ol>
+        <li>If you know what workflow you have run and have some results but 
+        don&#39;t know what you can plot from these results, browse the table 
+        below by different workflows, where you can see what data types there 
+        are in each workflow. By clicking the data type in the table, you will 
+        jump to the corresponding data preparation tab and you will see what 
+        plots you can make.</li>
+        <li>If you already know what plots you want, browse and click 
+        the gallery below and you will jump to the plotting tab directly. You 
+        will see what input data type this plot is accepting. By clicking your 
+        desired data type, you will go to a data prep tab and once you have 
+        prepared the required data, you will be guided back to this plotting tab.
+        </li>
+        </ol>
+             '),
         br(),
         p("DNAAseq: "),
         a(href = "#shiny-tab-df_raw",  "Raw Variant"),
         a(href = "",  "df2"),
         a(href = "",  "df3"),
-        gallery(title = "Gallery of Plots", texts = gallery_plot$texts, hrefs = gallery_plot$hrefs, images = gallery_plot$images, image_size = 4)
+        gallery(title = "Gallery of Plots", texts = gallery_plot$texts, hrefs = gallery_plot$hrefs, images = gallery_plot$images, image_frame_size = 3)
     )
 }
 
