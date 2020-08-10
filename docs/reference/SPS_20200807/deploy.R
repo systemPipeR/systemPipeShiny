24cf14b03ca611135723b1c7249041d0491f245b
lz <lzhan140@ucr.edu>
2020-08-10 16:15:39 -0700

    try to fix page

--- docs/reference/SPS_20200807/deploy.R
+++
@@ -1,8 +0,0 @@
-# To deploy:
-options(repos = BiocManager::repositories())
-# getOption("repos")
-# rsconnect::deployApp(
-#     appName = "systemPipeShiny",
-#     account = "acc_name",
-#     lint = TRUE
-#     )
