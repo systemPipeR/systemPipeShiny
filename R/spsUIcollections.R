7deca2beecf2dbff4c37cbc184034991878239ed
lz <lzhan140@ucr.edu>
2020-08-13 16:23:00 -0700

    exported ui example

--- R/spsUIcollections.R
+++ R/spsUIcollections.R
@@ -422,7 +422,7 @@ dynamicFile <- function(id, title = "Select your file:",
 #' @description return some example UI elements can be toggled on plotting
 #' @param ns namespace function
 #'
-#' @return
+#' @return some UI
 #' @importFrom shinyAce aceEditor
 #' @importFrom shinydashboardPlus boxPlus
 #' @importFrom shinyWidgets dropdownButton tooltipOptions
