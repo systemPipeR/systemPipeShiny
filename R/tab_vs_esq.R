d5c69cd96ed16342980222a65fdfa7e1b84f4915
lz <lzhan140@ucr.edu>
2021-05-06 13:25:17 -0700

    fix bioc check

--- R/tab_vs_esq.R
+++ R/tab_vs_esq.R
@@ -203,7 +203,7 @@ vs_esqServer <- function(id, shared) {
                 text = div(
                     h3("Continue to make plots?"),
                     HTML("Or manually click <span class='text-info'>2. Make a ggplot</span> panel"),
-                    if(packageVersion("esquisse") <= "1.0.1") {
+                    if(utils::packageVersion("esquisse") <= "1.0.1") {
                         tags$b(
                             class = "text-danger", style = "display:block",
                             "There are known bugs of {esquisse}. If you see 'object
